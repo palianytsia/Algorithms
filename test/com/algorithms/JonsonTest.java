2606235716b4b1962ae47af32f64c09ef1ef8092
Ivan Palianytsia <ivan.palianytsia@gmail.com>
2013-01-20 21:46:15 +0200

    add test for Jonson's algorithm

---
+++ test/com/algorithms/JonsonTest.java
@@ -0,0 +1,50 @@
+package com.algorithms;
+
+import java.io.FileNotFoundException;
+import java.util.SortedMap;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.algorithms.datastructures.graph.DirectedGraph;
+
+public class JonsonTest {
+
+    private final String graphFileLocation = "test\\data\\jonson.txt";
+    private final String negativeCycleGraphFileLocation = "test\\data\\jonson-negative-cycle.txt";
+
+    @Test
+    public void testAllPairsShortestPaths() {
+        Double minShortestPathDistance = Double.POSITIVE_INFINITY;
+        try {
+            DirectedGraph g = DirectedGraph.fromFile(graphFileLocation);
+            SortedMap<Integer, SortedMap<Integer, Double>> allPairsShortestPaths = Jonson.allPairsShortestPaths(g);
+            if (allPairsShortestPaths != null) {
+                for (Integer u : allPairsShortestPaths.keySet()) {
+                    SortedMap<Integer, Double> uDistances = allPairsShortestPaths.get(u);
+                    for (Integer v : uDistances.keySet()) {
+                        Double distance = uDistances.get(v);
+                        if (distance < minShortestPathDistance) {
+                            minShortestPathDistance = distance;
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            Assert.fail(e.getMessage());
+        }
+        Assert.assertEquals(19d, minShortestPathDistance);
+    }
+
+    @Test
+    public void testNegativeCycleDetection() {
+        try {
+            DirectedGraph g = DirectedGraph.fromFile(negativeCycleGraphFileLocation);
+            SortedMap<Integer, SortedMap<Integer, Double>> allPairsShortestPaths = Jonson.allPairsShortestPaths(g);
+            Assert.assertNull(allPairsShortestPaths);
+        } catch (FileNotFoundException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+}
