20de2c933a94c549c48d84692b1320743bda6c97
Ivan Palianytsia <ivan.palianytsia@gmail.com>
2013-01-20 19:12:49 +0200

    add several algorithms on graphs

---
+++ src/com/algorithms/Jonson.java
@@ -0,0 +1,72 @@
+package com.algorithms;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import com.algorithms.datastructures.graph.DirectedGraph;
+import com.algorithms.datastructures.graph.Edge;
+import com.algorithms.dynamic.BellmanFord;
+import com.algorithms.greedy.Dijkstra;
+
+/**
+ * Johnson's algorithm is a way to find the shortest paths between all pairs of vertices in a sparse directed graph. It
+ * allows some of the edge weights to be negative numbers, but no negative-weight cycles may exist. It works by using
+ * the Bellman–Ford algorithm to compute a transformation of the input graph that removes all negative weights, allowing
+ * Dijkstra's algorithm to be used on the transformed graph. It is named after Donald B. Johnson, who first published
+ * the technique in 1977. [From Wikipedia, the free encyclopedia].
+ * 
+ * @see <a href="https://class.coursera.org/algo2-2012-001">Algorithms: Design and Analysis, Part 2</a> by Tim
+ *      Roughgarden for detailed algorithm description, analysis and pseudo code.
+ * 
+ * @author Ivan Palianytsia <a href="mailto:ivan.palianytsia@gmail.com">ivan.palianytsia@gmail.com</a>
+ */
+public class Jonson {
+
+    public static SortedMap<Integer, SortedMap<Integer, Double>> allPairsShortestPaths(DirectedGraph g) {
+        SortedMap<Integer, SortedMap<Integer, Double>> distances = new TreeMap<Integer, SortedMap<Integer, Double>>();
+
+        // STEP 1: Form G' by adding a new vertex s and a new edge (s, v) with length 0 for each vertex v of G.
+        DirectedGraph gPrime = new DirectedGraph(g);
+        Integer s = gPrime.addVertex();
+        for (Integer v : gPrime.getVertices()) {
+            if (s != v) {
+                gPrime.addEdge(s, v, 0);
+            }
+        }
+
+        // STEP 2: Run Bellman-Ford on G' with source vertex s. If Bellman-Ford detects negative cost cycle in G' (which
+        // must lay in G), halt and report this (return null), else for each vertex v of V define p_v = length of the
+        // shortest s -> v path in G'.
+        SortedMap<Integer, Double> pValues = BellmanFord.shortestPath(gPrime, s);
+        if (pValues == null) {
+            return null;
+        }
+
+        // STEP 3: For each edge e = (u, v) of G define new cost c'_e = c_e + p_u - p_v
+        for (Edge e: g.getEdges()) {
+            double cPrime = e.getLength() + pValues.get(e.getVertexA()) - pValues.get(e.getVertexB());
+            e.setLength(cPrime);
+        }
+
+        // STEP 4: For each vertex u of G: Run Dijkstra's algorithm in G, with edge lengths {c'_e}, with source vertex
+        // u, to compute the shortest-path distance d'(u, v) for each v of G. Then compute the shortest-path distance
+        // d(u, v) = d'(u, v) - p_u + p_v, which is correct shortest-path distance for initial edge costs c.
+        for (Integer u : g.getVertices()) {
+            SortedMap<Integer, Double> uDistances = Dijkstra.shortestPath(g, u);
+            for (Integer v : uDistances.keySet()) {
+                double d = uDistances.get(v) - pValues.get(u) + pValues.get(v);
+                uDistances.put(v, d);
+            }
+            distances.put(u, uDistances);
+        }
+
+        // STEP 5: restore initial edge costs in G.
+        for (Edge e : g.getEdges()) {
+            double c = e.getLength() - pValues.get(e.getVertexA()) + pValues.get(e.getVertexB());
+            e.setLength(c);
+        }
+
+        return distances;
+    }
+
+}
---
+++ src/com/algorithms/greedy/Dijkstra.java
@@ -0,0 +1,56 @@
+package com.algorithms.greedy;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import com.algorithms.datastructures.graph.Edge;
+import com.algorithms.datastructures.graph.Graph;
+
+/**
+ * Dijkstra's shortest-path algorithm, surely one of the greatest hits of algorithms. It works in any directed or
+ * undirected graph with non-negative edge lengths, and it computes the shortest paths from a source vertex to all other
+ * vertices. Particularly nice is the blazingly fast implementation that uses a heap data structure.
+ * 
+ * @see <a href="https://class.coursera.org/algo-2012-002">Algorithms: Design and Analysis, Part 1</a> by Tim
+ *      Roughgarden for detailed algorithm description, analysis and pseudo code.
+ * 
+ * @author Ivan Palianytsia <a href="mailto:ivan.palianytsia@gmail.com">ivan.palianytsia@gmail.com</a>
+ */
+public class Dijkstra {
+    public static SortedMap<Integer, Double> shortestPath(Graph g, int source) {
+        final SortedMap<Integer, Double> processed = new TreeMap<Integer, Double>();
+        final Map<Integer, Double> greedyScores = new HashMap<Integer, Double>();
+        greedyScores.put(source, 0d);
+        for (Integer v : g.getVertices()) {
+            if (v != source) {
+                greedyScores.put(v, Double.POSITIVE_INFINITY);
+            }
+        }
+        final PriorityQueue<Integer> remaining = new PriorityQueue<Integer>(g.getNumVertices(),
+                new Comparator<Integer>() {
+            @Override
+            public int compare(Integer v, Integer w) {
+                return greedyScores.get(v).compareTo(greedyScores.get(w));
+            }
+        });
+        remaining.addAll(g.getVertices());
+        while (remaining.size() > 0) {
+            Integer vertex = remaining.poll();
+            Double distance = greedyScores.get(vertex);
+            processed.put(vertex, distance);
+            for (Edge e : g.getOutgoingEdges(vertex)) {
+                if (remaining.contains(e.getVertexB())) {
+                    Double oldKey = greedyScores.get(e.getVertexB());
+                    greedyScores.put(e.getVertexB(), Math.min(oldKey, distance + e.getLength()));
+                    remaining.remove(e.getVertexB());
+                    remaining.add(e.getVertexB());
+                }
+            }
+        }
+        return processed;
+    }
+}
