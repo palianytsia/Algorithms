de039b2ea4dc3f50d34fc990ae0ef73aff816cfb
Ivan Palianytsia <ivan.palianytsia@gmail.com>
2013-01-20 21:33:58 +0200

    no need to construct both graph before every test

--- test/com/algorithms/greedy/DijkstraTest.java
+++ test/com/algorithms/greedy/DijkstraTest.java
@@ -1,10 +1,10 @@
 package com.algorithms.greedy;
 
+import java.io.FileNotFoundException;
 import java.util.SortedMap;
 
 import junit.framework.Assert;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.algorithms.datastructures.graph.DirectedGraph;
@@ -12,33 +12,34 @@ import com.algorithms.datastructures.graph.UndirectedGraph;
 
 public class DijkstraTest {
 
-    private DirectedGraph directedGraph = null;
-    private UndirectedGraph undirectedGraph = null;
-
     private final String dataFileLocation = "test\\data\\greedy\\dijkstra.txt";
-
-    @Before
-    public void setUp() throws Exception {
-        directedGraph = DirectedGraph.fromFile(dataFileLocation);
-        undirectedGraph = UndirectedGraph.fromFile(dataFileLocation);
-    }
 
     @Test
     public void testDirectedGraphShortestPath() {
-        SortedMap<Integer, Double> distances = Dijkstra.shortestPath(directedGraph, 1);
-        Assert.assertEquals(10d, distances.get(2));
-        Assert.assertEquals(21d, distances.get(7));
-        Assert.assertEquals(39d, distances.get(9));
-        System.out.println(distances);
+        try {
+            DirectedGraph g = DirectedGraph.fromFile(dataFileLocation);
+            SortedMap<Integer, Double> distances = Dijkstra.shortestPath(g, 1);
+            Assert.assertEquals(10d, distances.get(2));
+            Assert.assertEquals(21d, distances.get(7));
+            Assert.assertEquals(39d, distances.get(9));
+            System.out.println(distances);
+        } catch (FileNotFoundException e) {
+            Assert.fail(e.getMessage());
+        }
     }
 
     @Test
     public void testUndirectedGraphShortestPath() {
-        SortedMap<Integer, Double> distances = Dijkstra.shortestPath(undirectedGraph, 1);
-        Assert.assertEquals(8d, distances.get(2));
-        Assert.assertEquals(19d, distances.get(7));
-        Assert.assertEquals(30d, distances.get(9));
-        System.out.println(distances);
+        try {
+            UndirectedGraph g = UndirectedGraph.fromFile(dataFileLocation);
+            SortedMap<Integer, Double> distances = Dijkstra.shortestPath(g, 1);
+            Assert.assertEquals(8d, distances.get(2));
+            Assert.assertEquals(19d, distances.get(7));
+            Assert.assertEquals(30d, distances.get(9));
+            System.out.println(distances);
+        } catch (FileNotFoundException e) {
+            Assert.fail(e.getMessage());
+        }
     }
 
 }
