8aff02cce07114e5c28dbd4d8e0272160d85da3b
Ivan Palianytsia <ivan.palianytsia@gmail.com>
2013-02-09 19:51:45 +0200

    missing file for improved clustering

---
+++ src/com/algorithms/greedy/Cluster.java
@@ -0,0 +1,72 @@
+package com.algorithms.greedy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.algorithms.datastructures.graph.Edge;
+import com.algorithms.datastructures.graph.Graph;
+
+public class Cluster {
+
+    private final Graph g;
+
+    private final Set<Integer> vertices = new HashSet<Integer>();
+
+    public Cluster(Graph g) {
+        this.g = g;
+    }
+
+    public void addVertex(int vertexId) {
+        vertices.add(vertexId);
+    }
+
+    public Double distanceTo(Cluster other) {
+        if (this.equals(other)) {
+            return 0d;
+        }
+        Double distance = Double.POSITIVE_INFINITY;
+        for (Edge e : getOutgoingEdges()) {
+            if ((other.vertices.contains(e.getVertexA()) || other.vertices.contains(e.getVertexB()))
+                    && e.getLength() < distance) {
+                distance = e.getLength();
+            }
+        }
+        return distance;
+    }
+
+    public Set<Integer> getVertices() {
+        return vertices;
+    }
+
+    public void merge(Cluster other) {
+        if (!this.g.equals(other.g)) {
+            throw new IllegalArgumentException();
+        }
+        this.vertices.addAll(other.vertices);
+    }
+
+    public void removeVertex(int vertexId) {
+        vertices.remove(vertexId);
+    }
+
+    public int size() {
+        return vertices.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Cluster [vertices=" + vertices + "]";
+    }
+
+    private Set<Edge> getOutgoingEdges() {
+        Set<Edge> outgoingEdges = new HashSet<Edge>();
+        for (Integer v : vertices) {
+            for (Edge e : g.getOutgoingEdges(v)) {
+                if (!vertices.contains(e.getOtherVertex(v))) {
+                    outgoingEdges.add(e);
+                }
+            }
+        }
+        return outgoingEdges;
+    }
+}
