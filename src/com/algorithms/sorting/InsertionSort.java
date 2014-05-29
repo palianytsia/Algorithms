9b92a294f41f9b0f60d9dfa8b07fead9b69fb3af
Ivan Palianytsia <ivan.palianytsia@gmail.com>
2014-05-30 01:29:16 +0200

    improve general code readabilty

--- src/com/algorithms/sorting/InsertionSort.java
+++ src/com/algorithms/sorting/InsertionSort.java
@@ -2,26 +2,35 @@ package com.algorithms.sorting;
 
 public class InsertionSort {
 
-    /**
-     * Sorts the array ascending using Insertion sort algorithm.
-     * 
-     * @param array
-     *            - array to be sorted.
-     */
-    public static void sort(int[] array) {
-        int comparisons = 0;
-        if (array.length > 1) {
-            for (int j = 1; j < array.length; j++) {
-                int i = j;
-                while (i > 0 && array[i] < array[i - 1]) {
-                    comparisons++;
-                    ArrayUtils.swap(array, i, i - 1);
-                    i--;
-                }
-            }
-        }
-        System.out.println("Array of " + array.length + " elements sorted via Insertion sort with " + comparisons
-                + " comparisons.");
-    }
+	/**
+	 * Sorts the array ascending using Insertion sort algorithm.
+	 * 
+	 * @param array
+	 *            - array to be sorted.
+	 */
+	public static void sort(int[] array) {
+		long comparisons = 0;
+		for (int j = 1; j < array.length; j++) {
+			comparisons += insert(array, j);
+		}
+		System.out.println("Array of " + array.length
+				+ " elements sorted via Insertion sort with " + comparisons
+				+ " comparisons.");
+	}
+
+	private static long insert(int[] array, int index) {
+		long comparisons = 0;
+		for (int i = index; i > 0; i--) {
+			comparisons++;
+			if (array[i - 1] > array[i]) {
+				ArrayUtils.swap(array, i, i - 1);
+			}
+			else {
+				// element is in its correct position
+				break;
+			}
+		}
+		return comparisons;
+	}
 
 }
