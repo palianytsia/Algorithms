e45a4a9b652e0e5384c70c84e927e8ff85dc9c77
Ivan Palianytsia <ivan.palianytsia@gmail.com>
2014-05-30 01:04:14 +0200

    do nothing when requested to swap an element with itself

--- src/com/algorithms/sorting/ArrayUtils.java
+++ src/com/algorithms/sorting/ArrayUtils.java
@@ -4,40 +4,42 @@ import java.util.List;
 
 public class ArrayUtils {
 
-    /**
-     * Swaps two elements of an array.
-     * 
-     * @param array
-     *            - the array to swap elements in.
-     * 
-     * @param a
-     *            - index of the first element to swap.
-     * 
-     * @param b
-     *            - index of the second element to swap.
-     */
-    public static void swap(int[] array, int a, int b) {
-        int temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
-    }
+	/**
+	 * Swaps two elements of an array.
+	 * 
+	 * @param array
+	 *            - the array to swap elements in.
+	 * 
+	 * @param a
+	 *            - index of the first element to swap.
+	 * 
+	 * @param b
+	 *            - index of the second element to swap.
+	 */
+	public static void swap(int[] array, int a, int b) {
+		if (a != b) {
+			int temp = array[a];
+			array[a] = array[b];
+			array[b] = temp;
+		}
+	}
 
-    /**
-     * Swaps two elements of a list.
-     * 
-     * @param array
-     *            - the list to swap elements in.
-     * 
-     * @param a
-     *            - index of the first element to swap.
-     * 
-     * @param b
-     *            - index of the second element to swap.
-     */
-    public static <T> void swap(List<T> list, int i, int j) {
-        T temp = list.get(i);
-        list.set(i, list.get(j));
-        list.set(j, temp);
-    }
+	/**
+	 * Swaps two elements of a list.
+	 * 
+	 * @param array
+	 *            - the list to swap elements in.
+	 * 
+	 * @param a
+	 *            - index of the first element to swap.
+	 * 
+	 * @param b
+	 *            - index of the second element to swap.
+	 */
+	public static <T> void swap(List<T> list, int i, int j) {
+		T temp = list.get(i);
+		list.set(i, list.get(j));
+		list.set(j, temp);
+	}
 
 }
