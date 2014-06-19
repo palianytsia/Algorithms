10362b8142379c5b34655dcbed8e312a0b7f059e
Ivan Palianytsia <ivan.palianytsia@gmail.com>
2014-06-20 00:03:23 +0200

    cleaner partion subroutine

--- src/com/algorithms/sorting/QuickSort.java
+++ src/com/algorithms/sorting/QuickSort.java
@@ -16,17 +16,15 @@ public class QuickSort {
 
     private static int partition(int[] array, int from, int to) {
 	int pivot = array[from];
-	int lessThanPivot = from;
-	int partitioned = from;
-	while (partitioned < to) {
-	    partitioned++;
-	    if (array[partitioned] <= pivot) {
-		lessThanPivot++;
-		ArrayUtils.swap(array, partitioned, lessThanPivot);
+	int newPivotIndex = from;
+	for (int j = from + 1; j <= to; j++) {
+	    if (array[j] <= pivot) {
+		ArrayUtils.swap(array, j, newPivotIndex + 1);
+		newPivotIndex++;
 	    }
 	}
-	ArrayUtils.swap(array, from, lessThanPivot);
-	return lessThanPivot;
+	ArrayUtils.swap(array, from, newPivotIndex);
+	return newPivotIndex;
     }
 
 }
